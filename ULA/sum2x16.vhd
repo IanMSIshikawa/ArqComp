6538aba9838b40ced29a500b397b385dcbed1cc5
IanMSIshikawa <ianishikawa1@gmail.com>
2024-04-13 17:46:14 -0300

    testbanch

--- ULA/sum2x16.vhd
+++ ULA/sum2x16.vhd
@@ -1,15 +1,13 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-use ieee.math_real.all;
+-- use ieee.math_real.all;
 
 entity sum2x16 is
     port (
         in_a: in unsigned (15 downto 0);
         in_b: in unsigned (15 downto 0);
-        result: out unsigned (15 downto 0);
-        carry: out std_logic;
-        overflow: out std_logic
+        result: out unsigned (15 downto 0)
     );
 end entity;
 
@@ -18,10 +16,6 @@ architecture a_sum2x16 of sum2x16 is
 signal bigResult: unsigned (16 downto 0);
 
 begin
-    bigResult <= in_a + in_b;
+    bigResult <= in_a+in_b;
     result <= bigResult (15 downto 0);
-    
-
-    
-
 end architecture;
