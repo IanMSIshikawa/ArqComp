63ff985e5fd76cbc65ba30a685fdd86a3f0a5462
IanMSIshikawa <ianishikawa1@gmail.com>
2024-06-14 21:18:24 -0300

    crivo sem otimização

--- microProcessador/uProcessador/uProcessador_tb.vhd
+++ microProcessador/uProcessador/uProcessador_tb.vhd
@@ -45,7 +45,7 @@ begin
     
     sim_time_proc: process
     begin
-        wait for 150 us;        
+        wait for 500 us;        
         finished <= '1';
         wait;
     end process sim_time_proc;
